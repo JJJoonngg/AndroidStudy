b4bc8e55a897649c03017d686f5660149bc427c8
JJong <whdtls3878@naver.com>
2020-02-17 14:45:10 +0900

    Email 인증 구현

---
+++ TestEmailSecond/app/src/main/java/com/example/testemailsecond/Utils.java
@@ -0,0 +1,9 @@
+package com.example.testemailsecond;
+
+public class Utils {
+    //This is your from email
+    public static final String EMAIL = "";
+
+    //This is your from email password
+    public static final String PASSWORD = "";
+}
