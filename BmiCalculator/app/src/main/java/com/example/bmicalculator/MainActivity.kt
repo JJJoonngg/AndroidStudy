77b08921252d80e0aa44b7775860cd16a65a334b
JJong <whdtls3878@naver.com>
2020-03-25 16:50:50 +0900

    마지막 입력내용 저장 및 가져오기

--- BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.bmicalculator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 
@@ -12,11 +13,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        resultButton.setOnClickListener{
+
+        loadData()
+
+        resultButton.setOnClickListener {
+
+            //Last Information save
+            saveData(
+                heightEditText.text.toString().toInt(),
+                weightEditText.text.toString().toInt()
+            )
+
             startActivity<ResultActivity>(
                 "weight" to weightEditText.text.toString(),
                 "height" to heightEditText.text.toString()
             )
+        }
+    }
+
+    private fun saveData(height: Int, weight: Int) {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+
+        editor.putInt("KEY_HEIGHT", height)
+            .putInt("KEY_WEIGHT", weight)
+            .apply()
+    }
+
+    private fun loadData(){
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val height = pref.getInt("KEY_HEIGHT",0)
+        val weight = pref.getInt("KEY_WEIGHT", 0)
+
+        if(height != 0 && weight !=0){
+            heightEditText.setText(height.toString())
+            weightEditText.setText(weight.toString())
         }
     }
 }
