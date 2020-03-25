f615783839af9173fa5b1a0d8bd67fdaa4fa9c56
JJong <whdtls3878@naver.com>
2020-03-25 16:36:26 +0900

    anko 라이브러리를 활용한 화면 전환

--- BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.bmicalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resultButton.setOnClickListener{
+            startActivity<ResultActivity>()
+        }
     }
 }
