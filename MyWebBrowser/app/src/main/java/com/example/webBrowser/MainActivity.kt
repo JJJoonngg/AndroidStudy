5a6aba91e7ccadb3f36a2de305719d1305d91034
JJong <whdtls3878@naver.com>
2020-03-30 17:02:34 +0900

    기본 브라우저 기능 작성

--- MyWebBrowser/app/src/main/java/com/example/webBrowser/MainActivity.kt
+++ MyWebBrowser/app/src/main/java/com/example/webBrowser/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.webBrowser
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        webView.apply {
+            settings.javaScriptEnabled = true
+            webChromeClient = WebViewClient()
+        }
+
+        webView.loadUrl("http://www.google.com")
+
+        urlEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                webView.loadUrl(urlEditText.text.toString())
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
