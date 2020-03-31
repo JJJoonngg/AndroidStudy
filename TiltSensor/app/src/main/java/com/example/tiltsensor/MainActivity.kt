8abd784acd23d437d8fd3ce4179c8acf20407b65
JJong <whdtls3878@naver.com>
2020-03-31 23:35:14 +0900

    미구현 method 추가 & Sensor Value 읽기

--- TiltSensor/app/src/main/java/com/example/tiltsensor/MainActivity.kt
+++ TiltSensor/app/src/main/java/com/example/tiltsensor/MainActivity.kt
@@ -2,12 +2,30 @@ package com.example.tiltsensor
 
 import android.content.Context
 import android.hardware.Sensor
+import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() , SensorEventListener{
+
+    //Call if sensor accuracy changed
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        //Call if sensor value changed
+        //values[0] : x value : up -10 ~ 0, down 0 ~ 10
+        //values[1] : y value : left -10 ~ 0, right 0 ~ 10
+        //values[2] : z not used
+        event?.let{
+            Log.d("MainActivity", "onSensorChanged: x: ${event.values[0]} , y: ${event.values[1]}, z: ${event.values[2]}")
+        }
+    }
 
     private val sensorManager by lazy {
         getSystemService(Context.SENSOR_SERVICE) as SensorManager
