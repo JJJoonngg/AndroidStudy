0c9fae8320e1aaa81dafeedd2a39d32f58247873
JJong <whdtls3878@gmail.com>
2020-04-08 13:34:15 +0900

    프래그먼트 기본 코드 수정

--- MyGallery/app/src/main/java/com/example/mygallery/PhotoFragment.kt
+++ MyGallery/app/src/main/java/com/example/mygallery/PhotoFragment.kt
@@ -6,11 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
+private const val ARG_URI = "uri"
+
 class PhotoFragment : Fragment() {
+    private var uri: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            uri = it.getString(ARG_URI)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_photo, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(uri: String) {
+            PhotoFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_URI, uri)
+                }
+            }
+        }
     }
 }
