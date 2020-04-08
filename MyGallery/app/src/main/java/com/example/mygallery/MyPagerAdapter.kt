1e55e2002f2fc7f071c08c24a4daf1f3d4cae2e0
JJong <whdtls3878@gmail.com>
2020-04-08 15:07:35 +0900

    코드 추가

---
+++ MyGallery/app/src/main/java/com/example/mygallery/MyPagerAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.mygallery
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class MyPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+    //뷰페이저가 표시할 프래그먼트 목록
+    private val items = ArrayList<Fragment>()
+
+    //position 위치의 프래그먼트
+    override fun getItem(position: Int): Fragment {
+        return items[position]
+    }
+
+    //아이템 개수
+    override fun getCount(): Int {
+        return items.size
+    }
+
+    //아이템 갱신
+    fun updateFragments(items: List<Fragment>) {
+        this.items.addAll(items)
+    }
+}
