f07c6f107a950c8f727e3a5df1a283f973479a20
JJong <whdtls3878@gmail.com>
2020-04-08 12:52:17 +0900

    모든 사진을 가져오는 코드 작성

--- MyGallery/app/src/main/java/com/example/mygallery/MainActivity.kt
+++ MyGallery/app/src/main/java/com/example/mygallery/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.mygallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getAllPhotos()
+    }
+
+    private fun getAllPhotos() {
+        //모든 사진 정보 가져오기
+        val cursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null,
+                null,
+                null,
+                MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC")
     }
 }
