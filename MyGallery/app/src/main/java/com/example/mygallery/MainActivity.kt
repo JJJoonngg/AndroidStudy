a38649f2b19d2ab228130bd648a9e78752ba2d2f
JJong <whdtls3878@gmail.com>
2020-04-08 13:08:48 +0900

    권한 요청

--- MyGallery/app/src/main/java/com/example/mygallery/MainActivity.kt
+++ MyGallery/app/src/main/java/com/example/mygallery/MainActivity.kt
@@ -1,16 +1,48 @@
 package com.example.mygallery
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.yesButton
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_READ_EXTERNAL_STORAGE = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getAllPhotos()
+        //권한이 부여되었는지 확인
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            //권한이 허용되지 않음
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                //이전에 이미 권한이 거부되었을 때 설명
+                alert("사진 정보를 얻으려면 외부 저장소 권한이 필수로 필요합니다", "권한이 필요한 이유") {
+                    yesButton {
+                        //권한 요청
+                        ActivityCompat.requestPermissions(this@MainActivity,
+                                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                                REQUEST_READ_EXTERNAL_STORAGE)
+                    }
+                    noButton { }
+                }.show()
+            } else {
+                //권한 요청
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                        REQUEST_READ_EXTERNAL_STORAGE)
+            }
+        }else{
+            //권한이 이미 허용됨
+            getAllPhotos()
+        }
     }
 
     private fun getAllPhotos() {
