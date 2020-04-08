de2905f74f1c711efb9f6dd76ac833cf237592b9
JJong <whdtls3878@gmail.com>
2020-04-08 13:35:21 +0900

    사진을 imageView에 표시

--- MyGallery/app/src/main/java/com/example/mygallery/PhotoFragment.kt
+++ MyGallery/app/src/main/java/com/example/mygallery/PhotoFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_photo.*
 
 private const val ARG_URI = "uri"
 
@@ -22,6 +24,11 @@ class PhotoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_photo, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Glide.with(this).load(uri).into(imageView)
     }
 
     companion object {
