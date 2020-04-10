4c10cccd72d8b62f443122e323842436ebc53708
JJong <whdtls3878@gmail.com>
2020-04-10 22:15:11 +0900

    Modify code Using Service

--- Flashlight/app/src/main/java/com/example/flashlight/MainActivity.kt
+++ Flashlight/app/src/main/java/com/example/flashlight/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.flashlight
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.intentFor
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,9 +15,9 @@ class MainActivity : AppCompatActivity() {
 
         flashSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
             if (isChecked) {
-                torch.flashOn()
+                startService(intentFor<TorchService>().setAction("on"))
             } else {
-                torch.flashOff()
+                startService(intentFor<TorchService>().setAction("off"))
             }
         }
     }
