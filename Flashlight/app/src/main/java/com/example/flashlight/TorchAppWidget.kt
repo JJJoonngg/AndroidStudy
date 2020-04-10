e151b6e36fd22485d276471240f44e1b26992718
JJong <whdtls3878@gmail.com>
2020-04-10 22:24:42 +0900

    Widget Click Event add

--- Flashlight/app/src/main/java/com/example/flashlight/TorchAppWidget.kt
+++ Flashlight/app/src/main/java/com/example/flashlight/TorchAppWidget.kt
@@ -1,8 +1,10 @@
 package com.example.flashlight
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
 
 class TorchAppWidget : AppWidgetProvider() {
@@ -24,18 +26,28 @@ class TorchAppWidget : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
     }
-}
 
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.torch_app_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    companion object {
 
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
+        internal fun updateAppWidget(
+            context: Context,
+            appWidgetManager: AppWidgetManager,
+            appWidgetId: Int
+        ) {
+            val widgetText = context.getString(R.string.appwidget_text)
+            // Construct the RemoteViews object
+            val views = RemoteViews(context.packageName, R.layout.torch_app_widget)
+            views.setTextViewText(R.id.appwidget_text, widgetText)
+
+            //실행할 Intent 작성
+            val intent = Intent(context, TorchService::class.java)
+            val pendingIntent = PendingIntent.getService(context, 0, intent, 0)
+
+            //위젯을 클릭하면 위에서 정의한 Intent 실행
+            views.setOnClickPendingIntent(R.id.appwidget_layout, pendingIntent)
+
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
 }
