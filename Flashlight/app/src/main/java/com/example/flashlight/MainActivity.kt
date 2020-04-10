72f9ac6502df1724b479b5c28ce5c115adb0950a
JJong <whdtls3878@gmail.com>
2020-04-10 22:07:55 +0900

    Flash On/Off code

--- Flashlight/app/src/main/java/com/example/flashlight/MainActivity.kt
+++ Flashlight/app/src/main/java/com/example/flashlight/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.flashlight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val torch = Torch(this)
+
+        flashSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                torch.flashOn()
+            } else {
+                torch.flashOff()
+            }
+        }
     }
 }
