02e88ecb8465142fb86feac9c177b4f8d136b0e3
JJong <whdtls3878@gmail.com>
2020-04-15 18:44:21 +0900

    코드 수정

--- TodoList/app/src/main/java/com/example/todolist/MainActivity.kt
+++ TodoList/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.todolist
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,9 +14,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fab.setOnClickListener {
+            startActivity<EditActivity>()
         }
     }
 
