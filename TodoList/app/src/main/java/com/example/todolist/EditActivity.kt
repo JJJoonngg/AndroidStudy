cf9252cad6a8698fc7efe5c1b59bb1a22e2afa8a
JJong <whdtls3878@gmail.com>
2020-04-15 18:22:13 +0900

    Realm 객체의 인스턴스 얻기

--- TodoList/app/src/main/java/com/example/todolist/EditActivity.kt
+++ TodoList/app/src/main/java/com/example/todolist/EditActivity.kt
@@ -2,11 +2,19 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.realm.Realm
 
 class EditActivity : AppCompatActivity() {
+
+    val realm = Realm.getDefaultInstance() // 인스턴스 얻기
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close() // 인스턴스 해제
     }
 }
