1aea70b46580b235073801f14fdcf5a27f357b60
JJong <whdtls3878@gmail.com>
2020-06-04 15:30:49 +0900

    Proejct Create

---
+++ exoplayerExample/app/src/main/java/com/jjjoonngg/exoplayerexample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jjjoonngg.exoplayerexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
