9e7750fbf8a676edde3ee2d1a1034427845e7d54
JJong <whdtls3878@gmail.com>
2020-06-04 16:18:22 +0900

    MainActivity 작성

--- exoplayerExample/app/src/main/java/com/jjjoonngg/exoplayerexample/MainActivity.kt
+++ exoplayerExample/app/src/main/java/com/jjjoonngg/exoplayerexample/MainActivity.kt
@@ -1,12 +1,54 @@
 package com.jjjoonngg.exoplayerexample
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.trackselection.TrackSelector
+import com.google.android.exoplayer2.upstream.BandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var exoPlayer: SimpleExoPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        try {
+            var bandwidthMeter: BandwidthMeter = DefaultBandwidthMeter()
+            var trackSelector: TrackSelector =
+                DefaultTrackSelector(AdaptiveTrackSelection.Factory(bandwidthMeter))
+            exoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector)
+
+            val videoURI = Uri.parse(VIDEO_URL)
+
+            val dataSourceFactory = DefaultHttpDataSourceFactory("ExoPlayer_VIDEO")
+            val extractorsFactory = DefaultExtractorsFactory()
+            val mediaSource = ExtractorMediaSource(videoURI, dataSourceFactory, extractorsFactory, null, null)
+
+            exo_player_view.setPlayer(exoPlayer)
+            exoPlayer.prepare(mediaSource)
+            exoPlayer.setPlayWhenReady(true)
+        }catch (exception:Exception){
+            Log.e("MainActivity", "Error : " + exception.toString())
+        }
+    }
+
+    companion object {
+        private const val VIDEO_URL =
+            "https://sites.google.com/site/ubiaccessmobile/sample_video.mp4"
     }
 }
