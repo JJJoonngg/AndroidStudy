b8947476bdbd18d03eaaf7c37403b7a0954db25d
JJJoonngg <whdtls3878@gmail.com>
2020-10-29 22:21:10 +0900

    android-youtube-api 단순 재생 테스트

--- Youtube_api_test/app/src/main/java/kr/co/jjjoonngg/youtube_api_test/MainActivity.kt
+++ Youtube_api_test/app/src/main/java/kr/co/jjjoonngg/youtube_api_test/MainActivity.kt
@@ -2,10 +2,28 @@ package kr.co.jjjoonngg.youtube_api_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val youTubePlayerView: YouTubePlayerView = findViewById(R.id.video_view)
+
+        lifecycle.addObserver(youTubePlayerView)
+        youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+                super.onReady(youTubePlayer)
+                val videoId: String = ""
+                youTubePlayer.loadVideo(videoId, 0F)
+            }
+
+            override fun onCurrentSecond(youTubePlayer: YouTubePlayer, second: Float) {
+                super.onCurrentSecond(youTubePlayer, second)
+            }
+        })
     }
 }
