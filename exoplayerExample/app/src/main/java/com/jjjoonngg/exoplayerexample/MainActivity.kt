0289cc0cbeb46bc43187a03edfb1667daf2c72d1
JJJoonngg <whdtls3878@gmail.com>
2020-11-01 23:31:55 +0900

    exoplayer 전체화면 구현 테스트 완료

--- exoplayerExample/app/src/main/java/com/jjjoonngg/exoplayerexample/MainActivity.kt
+++ exoplayerExample/app/src/main/java/com/jjjoonngg/exoplayerexample/MainActivity.kt
@@ -1,51 +1,151 @@
 package com.jjjoonngg.exoplayerexample
 
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.ExoPlayerFactory
+import android.os.PersistableBundle
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.DataBindingUtil
+import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
-import com.google.android.exoplayer2.source.ExtractorMediaSource
-import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.trackselection.TrackSelector
-import com.google.android.exoplayer2.upstream.BandwidthMeter
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
-import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.toast
-import java.lang.Exception
+import com.jjjoonngg.exoplayerexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var exoPlayer: SimpleExoPlayer
+    private val context: Context = this
+    private lateinit var binding: ActivityMainBinding
+
+    private var player: SimpleExoPlayer? = null
+    private var currentWindow: Int = 0
+    private var playBackPosition: Long = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
 
-        try {
-            var bandwidthMeter: BandwidthMeter = DefaultBandwidthMeter()
-            var trackSelector: TrackSelector =
-                DefaultTrackSelector(AdaptiveTrackSelection.Factory(bandwidthMeter))
-            exoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector)
+    override fun onResume() {
+        super.onResume()
+        initPlayer()
+    }
 
-            val videoURI = Uri.parse(VIDEO_URL)
+    override fun onRestart() {
+        super.onRestart()
+        initPlayer()
+    }
 
-            val dataSourceFactory = DefaultHttpDataSourceFactory("ExoPlayer_VIDEO")
-            val extractorsFactory = DefaultExtractorsFactory()
-            val mediaSource = ExtractorMediaSource(videoURI, dataSourceFactory, extractorsFactory, null, null)
-
-            exo_player_view.setPlayer(exoPlayer)
-            exoPlayer.prepare(mediaSource)
-            exoPlayer.setPlayWhenReady(true)
-        }catch (exception:Exception){
-            Log.e("MainActivity", "Error : " + exception.toString())
+    private fun initPlayer() {
+        if (player == null) {
+            player = SimpleExoPlayer.Builder(context).build()
+            binding.videoView.player = player
+        }
+        with(player!!) {
+            setMediaItem(MediaItem.fromUri(Uri.parse(VIDEO_URL)))
+            with(binding) {
+                videoView.setControllerOnFullScreenModeChangedListener {
+                    if (checkLandscapeOrientation()) {
+                        changeOrientationToLandscape(false)
+                    } else {
+                        changeOrientationToLandscape(true)
+                    }
+                }
+            }
+            prepare()
+            seekTo(currentWindow, playBackPosition)
+            playWhenReady = false
         }
     }
+
+    private fun releasePlayer() {
+        if (player != null) {
+            with(player!!) {
+                binding.videoView.player = null
+                playBackPosition = currentPosition
+                currentWindow = currentWindowIndex
+                playWhenReady = false
+                release()
+            }
+            player = null
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            hideSystemUI()
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        } else {
+            showSystemUI()
+            window.clearFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+            val params =
+                binding.videoView.layoutParams as ConstraintLayout.LayoutParams
+            params.width = ViewGroup.LayoutParams.MATCH_PARENT
+            params.height =
+                (250 * binding.videoView.resources.displayMetrics.density).toInt()
+            binding.videoView.layoutParams = params
+        }
+    }
+
+    private fun hideSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+
+    private fun showSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+    }
+
+    private fun checkLandscapeOrientation(): Boolean {
+        val orientation = resources.configuration.orientation
+        return orientation == Configuration.ORIENTATION_LANDSCAPE
+    }
+
+    private fun changeOrientationToLandscape(shouldLandScape: Boolean) {
+        requestedOrientation = if (shouldLandScape) {
+            ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        } else {
+            ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+    }
+
+    override fun onBackPressed() {
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            changeOrientationToLandscape(false)
+        } else {
+            super.onBackPressed()
+            releasePlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        releasePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        releasePlayer()
+    }
+
 
     companion object {
         private const val VIDEO_URL =
